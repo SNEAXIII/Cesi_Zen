36c737a9000b04c1aec49b6a6ad5a3738a623695
Sneaxiii <misterbalise2@gmail.com>
2025-06-26 16:16:38 +0200

    Add login

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,17 +1,24 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import SideNavBar from '@/app/ui/left-nav-bar/sidenav';
+import { Providers } from './providers';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+interface RootLayoutProps {
+  readonly children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang='en'>
+    <html lang='fr'>
       <body className={`${inter.className} antialiased`}>
-        <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-          <div className='w-full flex-none md:w-64'>
-            <SideNavBar />
+        <Providers>
+          <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
+            <div className='w-full flex-none md:w-64'>
+              <SideNavBar />
+            </div>
+            <div className='grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
           </div>
-          <div className='grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
-        </div>
+        </Providers>
       </body>
     </html>
   );
