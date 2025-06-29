9698bc38de20ab4f20bb013155c4335b12efbb0d
SNEAXIII <misterbalise2@gmail.com>
2025-06-29 14:45:46 +0200

    Add backend can get list of articles

--- api/src/dto/dto_articles.py
+++ api/src/dto/dto_articles.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic import BaseModel
 
 
@@ -5,3 +6,20 @@ class CreateArticle(BaseModel):
     title: str
     content: str
     category: int
+
+
+class GetArticleResponseMin(BaseModel):
+    id: int
+    title: str
+    id_category: int
+    creator: str
+    category: str
+
+
+class GetArticleResponseFull(GetArticleResponseMin):
+    content: str
+
+
+class GetAllArticleResponse(BaseModel):
+    articles: List[GetArticleResponseMin]
+    count: int
