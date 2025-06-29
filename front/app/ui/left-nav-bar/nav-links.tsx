d66a5b39a2a88e55c25d80c44e7f71e9afe82209
SNEAXIII <misterbalise2@gmail.com>
2025-06-29 22:54:43 +0200

    Enhance display for article page

--- front/app/ui/left-nav-bar/nav-links.tsx
+++ front/app/ui/left-nav-bar/nav-links.tsx
@@ -49,7 +49,7 @@ export default function NavLinks({ userRole }: NavLinksProps) {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-100 text-blue-600': pathname.startsWith(link.href) && link.href !== '/' || pathname === link.href,
               }
             )}
           >
