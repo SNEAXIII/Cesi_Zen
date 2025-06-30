143e0b42dab15b170e1afde09a3fd37d4058d273
SNEAXIII <misterbalise2@gmail.com>
2025-06-30 19:15:16 +0200

    Add fixtures

--- api/src/models/Articles.py
+++ api/src/models/Articles.py
@@ -2,6 +2,8 @@ import uuid
 from datetime import datetime
 from typing import Optional, TYPE_CHECKING
 from sqlmodel import Field, Relationship, SQLModel
+from sqlalchemy import Column
+from sqlalchemy.types import Text
 
 if TYPE_CHECKING:
     from src.models.User import User
@@ -13,7 +15,7 @@ class Article(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
-    content:str
+    content: str = Field(sa_column=Column(Text,nullable=False))
     created_at: datetime = Field(default_factory=datetime.now)
     id_user: uuid.UUID = Field(foreign_key="user.id")
     id_category: int = Field(foreign_key="category.id")
