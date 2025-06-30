b418cea9af48f77f54d55dc96f13766a02bd6a55
SNEAXIII <misterbalise2@gmail.com>
2025-06-30 14:04:59 +0200

    Add breathing page

---
+++ front/app/breathing/page.tsx
@@ -0,0 +1,10 @@
+export default function BreathingPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Exercice de respiration</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        {/* TODO PAGE CONTENT */}
+      </div>
+    </div>
+  );
+}
