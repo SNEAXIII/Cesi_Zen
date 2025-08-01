21c1bca06d0c240ca629798a24dff552a8538d49
SNEAXIII <misterbalise2@gmail.com>
2025-08-01 15:13:24 +0200

    Update docker to handle env in prod

--- api/src/security/secrets.py
+++ api/src/security/secrets.py
@@ -1,22 +1,25 @@
 import os
+
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+api_file = "api.env"
+
+IS_PROD = os.getenv("MODE") == "prod"
 
 
 class Settings(BaseSettings):
-    MARIADB_DATABASE: str
-    MARIADB_USER: str
-    MARIADB_PASSWORD: str
-    MARIADB_ROOT_PASSWORD: str
-    MARIADB_PORT: int
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    BCRYPT_HASH_ROUND: int
-    model_config = SettingsConfigDict(env_file="api.env")
-
-    @property
-    def MARIADB_HOST(self) -> str:
-        return "mariadb" if os.getenv("MODE") == "prod" else "localhost"
+    MARIADB_DATABASE: str = Field(... if IS_PROD else "cesi-zen")
+    MARIADB_USER: str = Field(... if IS_PROD else "user")
+    MARIADB_PASSWORD: str = Field(... if IS_PROD else "password")
+    MARIADB_ROOT_PASSWORD: str = Field(... if IS_PROD else "rootpassword")
+    MARIADB_PORT: int = Field(... if IS_PROD else 3306)
+    MARIADB_HOST: str = Field("mariadb" if IS_PROD else "localhost")
+    SECRET_KEY: str = Field(... if IS_PROD else "dev-secret-key")
+    ALGORITHM: str = Field(... if IS_PROD else "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(... if IS_PROD else 60, le=60)
+    BCRYPT_HASH_ROUND: int = Field(... if IS_PROD else 8, ge=12 if IS_PROD else 8)
+    model_config = SettingsConfigDict(env_file=api_file)
 
 
 SECRET = Settings()
