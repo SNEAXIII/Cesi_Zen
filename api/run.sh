d1ad42526773b2b2c4b5f4f0fafd618e495da6fc
Alexis Bassignot <alexis.bassignot@flowbird.group>
2025-08-25 17:26:39 +0200

    Working ?

--- api/run.sh
+++ api/run.sh
@@ -1,4 +1,5 @@
-./wait-for-it.sh mariadb:3306
+set -e
+./wait-for-it.sh mariadb:3306 -t 60 --strict
 export MODE="prod"
 alembic upgrade head
 fastapi run
--- front/run.sh
+++ front/run.sh
@@ -1,3 +1,4 @@
-/bin/bash ./wait-for-it.sh api:8000
+set -e
+/bin/bash ./wait-for-it.sh api:8000 -t 60 --strict
 export NODE_ENV=production
 node server.js
