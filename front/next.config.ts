3c79becb65209ba52dcca4bd084fcd1eac05d082
SNEAXIII <misterbalise2@gmail.com>
2025-08-30 01:47:46 +0200

    Fix ip address

--- front/next.config.ts
+++ front/next.config.ts
@@ -5,6 +5,6 @@ const API_PORT = process.env.API_PORT ?? '8000';
 const nextConfig: NextConfig = {
   output: 'standalone',
 };
-export const CLIENT_API_URL: string = `http://${API_CLIENT_HOST}:${API_PORT}`;
+export const CLIENT_API_URL: string = `http://${API_CLIENT_HOST}/api/back/`;
 export const SERVER_API_URL: string = `http://${API_SERVER_HOST}:${API_PORT}`;
 export default nextConfig;
