400ade3996a153fa4b1f68996eea247b47351a35
SNEAXIII <misterbalise2@gmail.com>
2025-08-30 19:03:56 +0200

    Add first init and fix api

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 const API_SERVER_HOST = process.env.NODE_ENV === 'production' ? 'api' : 'localhost';
 const API_PORT = process.env.API_PORT ?? '8000';
 const API_CLIENT_HOST = process.env.API_CLIENT_HOST ?? 'localhost';
-const API_CLIENT_END_PART = process.env.NODE_ENV === 'production'? '/api/back/':`:${API_PORT}`;
+const API_CLIENT_END_PART = process.env.NODE_ENV === 'production'? '/api/back':`:${API_PORT}`;
 const nextConfig: NextConfig = {
   output: 'standalone',
 };
