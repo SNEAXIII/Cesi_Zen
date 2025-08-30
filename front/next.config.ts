a61ccc0a0e54e3e5e7e6bb80ef230f607dbdede9
Sneaxiii <misterbalise2@gmail.com>
2025-08-30 02:41:20 +0200

    Fix Caddyfile

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from 'next';
-const API_CLIENT_HOST = process.env.API_CLIENT_HOST ?? 'localhost';
 const API_SERVER_HOST = process.env.NODE_ENV === 'production' ? 'api' : 'localhost';
 const API_PORT = process.env.API_PORT ?? '8000';
+const API_CLIENT_HOST = process.env.API_CLIENT_HOST ?? 'localhost';
+const API_CLIENT_END_PART = process.env.NODE_ENV === 'production'? '/api/back/':`:${API_PORT}`;
 const nextConfig: NextConfig = {
   output: 'standalone',
 };
-export const CLIENT_API_URL: string = `http://${API_CLIENT_HOST}/api/back/`;
+export const CLIENT_API_URL: string = `http://${API_CLIENT_HOST}${API_CLIENT_END_PART}`;
 export const SERVER_API_URL: string = `http://${API_SERVER_HOST}:${API_PORT}`;
 export default nextConfig;
